620804fdabd5e732699ec0c47ff0ce8937ea3fc7
niranjanselvij <35652873+niranjanselvij@users.noreply.github.com>
2018-01-21 16:39:36 +0530

    Create bb.c

---
+++ bb.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+void main()
+{
+int a,b,N;
+printf("enter value of a");
+scanf("%d",&a);
+printf("enter value of b");
+scanf("%d",&b);
+N=a<b;
+N<100000?printf("positive"):printf("negative");
+}
+#include<stdio.h>
+void main()
+{
+int a,b,N;
+printf("enter value of a");
+scanf("%d",&a);
+printf("enter value of b");
+scanf("%d",&b);
+N=a<b;
+N<100000?printf("positive"):printf("negative");
+}
